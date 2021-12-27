3e9f87ab37cd3998c0eda1965dfe5cc77e9ce563
esysolina <evelina.ova@gmail.com>
2021-12-27 23:40:30 +0500

    added files

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void selenideTest () {
+        open("https://github.com/selenide/selenide");
+        $(byText("Wiki")).click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(Condition.text("JUnit5"));
+    }
+}
