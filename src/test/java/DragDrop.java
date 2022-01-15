2b88192ce8d374c7a130b23649dd669f8eeb2893
esysolina <evelina.ova@gmail.com>
2022-01-15 17:16:39 +0500

    added dragdrop test

--- src/test/java/DragDrop.java
+++ src/test/java/DragDrop.java
@@ -1,19 +1,17 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.*;
 
 public class DragDrop {
+
     @Test
     void dragDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
 
-//        SelenideElement element = $("#column-a");
-//        actions().moveToElement(element).click(element).perform();
-
-        $("#column-a").$("header").shouldHave(Condition.text("B"));
+        $("#column-b").shouldHave(Condition.text("A"));
+        $("#column-a").shouldHave(Condition.text("B"));
     }
 }
